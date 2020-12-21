c009c520634c5f1ac25e6c779ea65b58380c288d
luiz eduardo silva <luizscarpo@gmail.com>
2020-12-21 14:52:58 -0300

    Backend start

---
+++ backend/control-fin/src/main/java/br/com/code/luiz/controlfin/ControlFinApplication.java
@@ -0,0 +1,13 @@
+package br.com.code.luiz.controlfin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlFinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlFinApplication.class, args);
+	}
+
+}
---
+++ backend/control-fin/src/test/java/br/com/code/luiz/controlfin/ControlFinApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.code.luiz.controlfin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlFinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
