2bbdec36f9f9563c84ebcc7c9905ca13b7f9a70d
luiz eduardo silva <luizscarpo@gmail.com>
2020-12-21 15:40:16 -0300

    Add models

---
+++ backend/control-fin/src/main/java/br/com/code/luiz/controlfin/models/Control_fin.java
@@ -0,0 +1,62 @@
+package br.com.code.luiz.controlfin.models;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "TB_Control")
+public class Control_fin implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.AUTO )
+    private long id;
+
+    private String description;
+
+    private Date date;
+
+    private BigDecimal value;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+     
+}
